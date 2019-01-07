49a6963a5a8973665b1cb9deaea4412452adb636
Henrique Novais <henriquennoliveira@gmail.com>
2019-01-07 17:42:08 -0300

    Implement simple react app

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function transform(offset) {
+  const cos = Math.cos(offset);
+  const sin = Math.sin(offset);
+  return { transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)` };
 }
+
+class App extends Component {
+    state = { styleOne: {}, styleTwo: {} };
+  
+    onMouseMove = (event) => {
+      this.setState({
+        styleOne: transform(-event.clientX / event.clientY),
+        styleTwo: transform(event.clientX / event.clientY)
+      })
+    }
+   
+    render(){
+      return <div onMouseMove={this.onMouseMove}>
+        <div className="panel"  style={this.state.styleOne} />
+        <div classname="panel"  style={this.state.styleTwo} />
+      </div>  
+    }
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
+
 
 export default App;
