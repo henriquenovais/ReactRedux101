f7759951b05ab5feda6b4b1d9f1c3269a7cedd60
Henrique Novais <henriquennoliveira@gmail.com>
2019-01-16 20:22:18 -0300

    Add commeting on JSX vs HTML syntax differences with examples

--- reactbasics/src/index.js
+++ reactbasics/src/index.js
@@ -13,11 +13,25 @@ of JavaScript that you are using.
 
 // Create a react component
 /*
-A React component is a function or a class.
+A React component is a function or a class that has information
+that will be converted into HTML through Babel.
+The JavaScript that is used to create HTML components is called JSX.
 */
 
 const App = () => {
-    return <div><h1>Hi there!</h1></div>;
+    return (
+    <div>
+        <h1>Hello World!</h1>
+        <ul>
+            <li>Hey</li>
+            <li>HEY HEY</li>
+            <li>HEY HEY HEY HEY HEY HEY</li>
+        </ul>
+        <label class="label" for="name">Enter name: </label>
+        <input id="name"/>
+        <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+    </div>
+    );
 }
 
 // Take the react component and show it on the screen
