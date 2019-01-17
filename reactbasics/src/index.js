63cc2005ad744abae5fa3635673a4857f61d8ea6
Henrique Novais <henriquennoliveira@gmail.com>
2019-01-16 21:02:14 -0300

    Commeting on usage of JS variables on JSX

--- reactbasics/src/index.js
+++ reactbasics/src/index.js
@@ -18,7 +18,26 @@ that will be converted into HTML through Babel.
 The JavaScript that is used to create HTML components is called JSX.
 */
 
+
 const App = () => {
+    // const buttonText = "I'm blue";
+    /*
+    In order to refer to a JavaScript variable in JSX
+    it is necessary to use {}. So, when referring to a JavaScript
+    object in JSX it'll be necessary to use javaScriptObject.propertie.
+
+    EXAMPLE:
+    const object = {text: 'Hi there!'};
+    In order to refer to this object >>>>> AS TEXT <<<< in JSX it is required to do:
+    {object.text}
+
+    */ 
+    let buttonText;
+    // buttonText = 'Click me!';
+    // buttonText = ['Hi ', 'There!'];
+    // buttonText = 12345;
+    buttonText = {object: 'Click me!'};
+
     return (
     <div>
         <h1>Hello World!</h1>
@@ -27,9 +46,11 @@ const App = () => {
             <li>HEY HEY</li>
             <li>HEY HEY HEY HEY HEY HEY</li>
         </ul>
-        <label class="label" for="name">Enter name: </label>
+        <label className="label" htmlFor="name">Enter name: </label>
         <input id="name"/>
-        <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+        <button style={{backgroundColor: 'blue', color: 'white'}}>
+        {buttonText.object}
+        </button>
     </div>
     );
 }
