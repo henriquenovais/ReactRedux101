46f882c119bda627e8ef0bb0390823a530785b83
Henrique Novais <henriquennoliveira@gmail.com>
2019-01-16 22:06:34 -0300

    Create comment section with React

--- components/src/index.js
+++ components/src/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-
+//https://github.com/Marak/faker.js
 const App = () => {
     return(
     <div className="ui container comments">
+        <div className="comment">
+            <a href="/" className="avatar">
+                <img alt="avatar" src={faker.image.avatar()}/>
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                    Sam
+                </a>
+                <div className="metadata">
+                    <span className="date">Today at 6PM</span>
+                </div>
+                <div className="text">
+                        Nice blog post!!
+                </div>
+            </div>
+        </div>
+        <div className="comment">
+            <a href="/" className="avatar">
+                <img alt="avatar" src={faker.image.avatar()}/>
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                    Sam
+                </a>
+                <div className="metadata">
+                    <span className="date">Today at 6PM</span>
+                </div>
+                <div className="text">
+                        Nice blog post!!
+                </div>
+            </div>
+        </div>
         <div className="comment">
             <a href="/" className="avatar">
                 <img alt="avatar" src={faker.image.avatar()}/>
