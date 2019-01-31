0e64d9546078d8d69452602f1a19aa6efd479f02
Henrique Novais <henriquennoliveira@gmail.com>
2019-01-31 20:35:54 -0300

    Add commenting on class and function based components

--- components/src/index.js
+++ components/src/index.js
@@ -13,6 +13,21 @@ const App = () => {
   When in doubt about which part of the props is receiving the information, just do a 
   console.log(props); in order to sort out how the information is being passed through.
   */
+  /*
+  Class based components VS Function based components
+
+  There's a lot of debate in the React community about which are the cases to use
+  class based components and function based components, Stephen Grider is one of which
+  chooses to use Class based components in every possible ocasion except from when it is
+  necessary to display simple content --- when it comes to displaying simple content Stephen
+  thinks it is better to use function based components but he does recognizes the community debate.
+
+  Class based components are better for code organization, a single component can have multiple states
+  --- this kind of component easier to handle user input --- and this kind of component can understand
+  better life-cycle events. 
+
+
+ */
 
   return (
     <div className="ui container comments">
