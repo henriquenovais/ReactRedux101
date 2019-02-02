6e70e1b12ca92a14ab6c5a322a3223e06d9da76e
Henrique Novais <henriquennoliveira@gmail.com>
2019-02-01 22:09:32 -0300

    Move components base commenting and add two new component placeholders

---
+++ sillyweather/src/SeasonDisplay.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SeasonDisplay = () => {
+  return (
+    <div>
+      <h1>IT IS SUMMER, HOT AS FUCK</h1>
+    </div>
+  );
+};
+
+export default SeasonDisplay;
--- sillyweather/src/index.js
+++ sillyweather/src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*
+  Class based components VS Function based components
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  There's a lot of debate in the React community about which are the cases to use
+  class based components and function based components, Stephen Grider is one of which
+  chooses to use Class based components in every possible ocasion except from when it is
+  necessary to display simple content --- when it comes to displaying simple content Stephen
+  thinks it is better to use function based components but he does recognizes the community debate.
+
+  Class based components are better for code organization, a single component can have multiple states
+  --- this kind of component easier to handle user input --- and this kind of component can understand
+  better life-cycle events. 
+
+
+ */
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is JSX baby</h1>
+      <SeasonDisplay />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
