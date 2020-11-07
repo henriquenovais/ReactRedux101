93ccd166acc04836cd538989306fef9aa81cacf0
Henrique Novais <henriquennoliveira@gmail.com>
2020-11-06 22:38:44 -0300

    add accordion customization

--- react-hooks/src/components/Accordion.js
+++ react-hooks/src/components/Accordion.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 const Accordion = ({ items }) => {
+    const renderedItems = items.map(item => {
+            return <div key={item.title}>
+                
+
+
+            </div>
+        })
+
+    }
     return (
     <h1>{items.length}</h1>
     );
