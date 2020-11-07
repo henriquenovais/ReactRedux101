c339b4737392d666556d0a7c4e0befac9a6d58b2
Henrique Novais <henriquennoliveira@gmail.com>
2020-11-06 22:31:19 -0300

    passing props through App.js to Accordion.js

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is topzera!'
+  },
+  {
+    title: 'Is React good?',
+    content: 'Yes, React is a very good framework'
+  },
+  {
+    title: 'How good is React?',
+    content: 'Amazing, software engineers love it'
+  }
+];
 
 export default () => {
   return (
     <div>
-	<h1>react hooks practice</h1>
+	    <h1>react hooks practice</h1>
+      <Accordion items={items}/>
     </div>
   );
 }
---
+++ react-hooks/src/components/Accordion.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Accordion = ({ items }) => {
+    return (
+    <h1>{items.length}</h1>
+    );
+}
+
+export default Accordion;
