cc0701a3b06c0890c5c41bc0849ad4137a375742
Henrique Novais <henriquennoliveira@gmail.com>
2020-11-08 22:40:20 -0300

    implement react hook

--- react-hooks/src/components/Accordion.js
+++ react-hooks/src/components/Accordion.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Accordion = ({ items }) => {
+    const [activeIndex, setActiveIndex] = useState(null);
 
     const onTitleClick = (index) => {
-        console.log('Title clicked');
+        setActiveIndex(index);
     };
 
     const renderedItems = items.map((item, index) => {
             return <React.Fragment key={item.title}>
                 <div 
                     className="title active"
-                    onClick={() => console.log ('Title clicked', index)} 
+                    onClick={() => onTitleClick(index)} 
                     >
                     <i className="dropdown icon"></i>
                     {item.title}
@@ -21,8 +22,13 @@ const Accordion = ({ items }) => {
             </React.Fragment>
         })
     return (
-        <div className="ui styled accordion">
-            {renderedItems}
+        <div>
+            <div className="ui styled accordion">
+                {renderedItems}
+            </div>
+            <div>
+                <h1>{activeIndex}</h1>
+            </div>
         </div>
     );
 }
