e91bc00ee6624910ef51266a52377bc9bc6b079d
Henrique Novais <henriquennoliveira@gmail.com>
2020-11-08 22:20:32 -0300

    add accordion responsiveness

--- react-hooks/src/components/Accordion.js
+++ react-hooks/src/components/Accordion.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const Accordion = ({ items }) => {
-    const renderedItems = items.map(item => {
+
+    const onTitleClick = (index) => {
+        console.log('Title clicked');
+    };
+
+    const renderedItems = items.map((item, index) => {
             return <React.Fragment key={item.title}>
-                <div className="title active">
+                <div 
+                    className="title active"
+                    onClick={() => console.log ('Title clicked', index)} 
+                    >
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
