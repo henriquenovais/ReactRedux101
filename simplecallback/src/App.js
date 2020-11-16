fc53c84aa5221e4b174a2d61033b14991862fa57
Henrique Oliveira <henriquennoliveira@gmail.com>
2020-11-15 22:28:36 -0300

    add button component

--- simplecallback/src/App.js
+++ simplecallback/src/App.js
@@ -1,7 +1,10 @@
+import Button from './components/button';
+
 function App() {
   return (
     <div>
       <h1>First Run</h1>
+      <Button/>
     </div>
   );
 }
---
+++ simplecallback/src/components/button.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+const Button = () => {
+    return(
+        <div>
+            <button type="block">Click me!</button>
+        </div>
+    );
+}
+
+export default  Button;
