8cf157c506b1b159e983c72c53310ea48f944902
Henrique Novais <henriquennoliveira@gmail.com>
2020-12-05 20:47:22 -0300

    restructuring app

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,36 +1,18 @@
 import React from 'react';
-import Accordion from './components/Accordion';
+import ChangeBackground from './components/ChangeBackground';
 import './App.css';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is topzera!'
-  },
-  {
-    title: 'Is React good?',
-    content: 'Yes, React is a very good framework'
-  },
-  {
-    title: 'How good is React?',
-    content: 'Amazing, software engineers love it'
-  }
-];
-
-export default () => {
+const App = () => {
   return (
     <div>
       <div className="background">
       </div>
-      <div className="button-group">
-        <button type="button">CodeGeass</button> 
-        <button type="button">Naruto</button> 
-        <button type="button">Fullmetal</button> 
-        <button type="button">Avatar</button> 
-      </div>
+      <ChangeBackground/>
     </div>
 
 
   );
 }
+
+export default App;
 
---
+++ react-hooks/src/components/ChangeBackground.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ChangeBackground = () => {
+
+    return (
+    <div className="button-group">
+        <button type="button">CodeGeass</button> 
+        <button type="button">Naruto</button> 
+        <button type="button">Fullmetal</button> 
+        <button type="button">Avatar</button> 
+    </div>
+    );
+}
+
+export default ChangeBackground;
