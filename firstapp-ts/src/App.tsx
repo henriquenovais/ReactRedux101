1b7310a4f699924ddcc5fc603e6575507a1b6c07
Henrique <henriquennoliveira@gmail.com>
2023-04-12 20:43:00 -0300

    feat: simple firstapp ts version

---
+++ firstapp-ts/src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <div style={{ backgroundColor: "blue" }}>
+      <h1 style={{ color: "white" }}>This is the app component</h1>
+    </div>
+  );
+};
+
+export default App;
--- firstapp-ts/src/index.tsx
+++ firstapp-ts/src/index.tsx
@@ -1,7 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(<h1>Good stuff</h1>);
+const htmlRoot = document.getElementById("root") as HTMLElement;
+
+const root = ReactDOM.createRoot(htmlRoot);
+
+root.render(<App />);
