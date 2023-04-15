562fe75ae2abe4ace7816aeaac29fcb74e1ffd4e
Henrique <henriquennoliveira@gmail.com>
2023-04-14 21:30:13 -0300

    feat: configure image import into typescript

--- pdas-ts/src/App.tsx
+++ pdas-ts/src/App.tsx
@@ -2,12 +2,11 @@ import { FC } from "react";
 import ProfileCard from "./ProfileCard";
 import AlexaImage from "./images/alexa.png";
 
-console.log(AlexaImage);
-
 const App: FC = () => {
   return (
     <div>
       <h1>This is the PDAS app</h1>
+      <img src={AlexaImage} alt="" />
       <ProfileCard title="cortana" handle="@cortana22" />
       <ProfileCard title="siri" handle="@siri01" />
       <ProfileCard title="alexa" handle="@alexa99" />
