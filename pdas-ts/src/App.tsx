f8e73f2ba6362b1dbe80c117fb45d8a37e221cec
Henrique <henriquennoliveira@gmail.com>
2023-04-14 21:33:51 -0300

    feat: pdas profile card components

--- pdas-ts/src/App.tsx
+++ pdas-ts/src/App.tsx
@@ -1,15 +1,32 @@
 import { FC } from "react";
 import ProfileCard from "./ProfileCard";
 import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 const App: FC = () => {
   return (
     <div>
       <h1>This is the PDAS app</h1>
-      <img src={AlexaImage} alt="" />
-      <ProfileCard title="cortana" handle="@cortana22" />
-      <ProfileCard title="siri" handle="@siri01" />
-      <ProfileCard title="alexa" handle="@alexa99" />
+
+      <ProfileCard
+        title="cortana"
+        handle="@cortana22"
+        img={CortanaImage}
+        imgAlt="cortana profile img"
+      />
+      <ProfileCard
+        title="siri"
+        handle="@siri01"
+        img={SiriImage}
+        imgAlt="siri profile img"
+      />
+      <ProfileCard
+        title="alexa"
+        handle="@alexa99"
+        img={AlexaImage}
+        imgAlt="alexa profile img"
+      />
     </div>
   );
 };
--- pdas-ts/src/ProfileCard.tsx
+++ pdas-ts/src/ProfileCard.tsx
@@ -3,15 +3,16 @@ import { FC } from "react";
 interface IProfileCard {
   title: string;
   handle: string;
-  image?: string;
+  img: string;
+  imgAlt: string;
 }
 
-const ProfileCard: FC<IProfileCard> = ({ title, handle, image }) => {
+const ProfileCard: FC<IProfileCard> = ({ title, handle, img }) => {
   return (
     <>
       <h1>{title}</h1>
       <h2>{handle}</h2>
-      <>{image}</>
+      <img src={img} alt="" />
     </>
   );
 };
