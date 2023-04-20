23ad36e5dddc3e61070ca33cf98d7e776a34124a
Henrique <henriquennoliveira@gmail.com>
2023-04-19 22:02:28 -0300

    feat: use svg inside animals-ts project

--- animals-ts/src/components/AnimalShow.tsx
+++ animals-ts/src/components/AnimalShow.tsx
@@ -1,11 +1,18 @@
 import { FC } from "react";
 
+import { ReactComponent as CatSvg } from "../svg/cat.svg";
+
 interface IAnimalShow {
   type: string;
 }
 
 const AnimalShow: FC<IAnimalShow> = ({ type }) => {
-  return <h1>{type}</h1>;
+  return (
+    <div>
+      <CatSvg />
+      <h1>{type}</h1>
+    </div>
+  );
 };
 
 export default AnimalShow;
