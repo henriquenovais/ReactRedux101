50ac809c3ac52bc68fef963e0b2c908e2bd89f53
Henrique <henriquennoliveira@gmail.com>
2023-04-19 22:11:37 -0300

    feat: add css files and function to select svg according to animal type on animals-ts

--- animals-ts/src/components/AnimalShow.tsx
+++ animals-ts/src/components/AnimalShow.tsx
@@ -1,16 +1,39 @@
 import { FC } from "react";
 
 import { ReactComponent as CatSvg } from "../svg/cat.svg";
+import { ReactComponent as DogSvg } from "../svg/dog.svg";
+import { ReactComponent as HorseSvg } from "../svg/horse.svg";
+import { ReactComponent as CowSvg } from "../svg/cow.svg";
+import { ReactComponent as BirdSvg } from "../svg/bird.svg";
+import { ReactComponent as GatorSvg } from "../svg/gator.svg";
 
 interface IAnimalShow {
   type: string;
 }
 
 const AnimalShow: FC<IAnimalShow> = ({ type }) => {
+  const selectAnimal = (type: string): JSX.Element | undefined => {
+    switch (type) {
+      case "cat":
+        return <CatSvg />;
+      case "horse":
+        return <HorseSvg />;
+      case "cow":
+        return <CowSvg />;
+      case "bird":
+        return <BirdSvg />;
+      case "gator":
+        return <GatorSvg />;
+      case "dog":
+        return <DogSvg />;
+      default:
+        return undefined;
+    }
+  };
+
   return (
     <div>
-      <CatSvg />
-      <h1>{type}</h1>
+      <h1>{selectAnimal(type)}</h1>
     </div>
   );
 };
