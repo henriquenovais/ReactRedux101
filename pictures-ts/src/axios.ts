e4a4ba38a62a3a5c75a3613f533bcb7a2ce58747
Henrique <henriquennoliveira@gmail.com>
2023-04-27 22:02:39 -0300

    feat: testing js docs

--- pictures-ts/src/axios.ts
+++ pictures-ts/src/axios.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 
 interface IAxiosUser {
-  axiosGet(): void;
+  /**
+   * Some description
+   * 
+   *  @param {string} some param used to do a request
+   *  @returns {Promise<boolean>} returns true if request was successful
+   */
+  axiosGet(someParam: string): Promise<void>;
 }
 
 class Axio
