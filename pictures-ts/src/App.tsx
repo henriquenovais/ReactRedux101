0814c5f2b043894403fc72962d283d5cf60cb447
Henrique <henriquennoliveira@gmail.com>
2023-05-02 20:41:25 -0300

    feat: implement unsplash service

--- pictures-ts/src/App.tsx
+++ pictures-ts/src/App.tsx
@@ -1,6 +1,11 @@
 import { FC } from "react";
+import unsplashService from "./services/unsplash";
 
 const App: FC = () => {
+  unsplashService.getImage("cars").then((result) => {
+    console.log("result >>>>>>>>>", result);
+  });
+
   return <h1>App</h1>;
 };
 
--- pictures-ts/src/services/unsplash.ts
+++ pictures-ts/src/services/unsplash.ts
@@ -1,1 +1,15 @@
-export const Unsplash = () => {};
+import { HTTPClient } from "../axios";
+
+class UnsplashService extends HTTPClient {
+  getImage = async (searchTerm: string): Promise<any> => {
+    const url = `search/photos?query=${searchTerm}`;
+    const response = await this.get<any>(url);
+
+    return response.data;
+  };
+}
+
+const url = "https://api.unsplash.com/";
+const unsplashService = new UnsplashService(url);
+
+export default unsplashService;
