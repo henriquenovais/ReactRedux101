38815e7aa57ec45cf9904e4dae540cb76e63e435
Henrique <henriquennoliveira@gmail.com>
2023-05-17 21:29:45 -0300

    feat: add mui library, remove mui-icons

--- book-list-ts/src/components/BookCard.tsx
+++ book-list-ts/src/components/BookCard.tsx
@@ -10,8 +10,12 @@ const BookCard: FC<IBookCard> = ({ book }) => {
   return (
     <div className="book-card card">
       <h3>{book.title}</h3>
-      <button className="book-card button">Edit</button>
-      <button className="book-card button">Close</button>
+      <button className="book-card button">
+        <Edit />
+      </button>
+      <button className="book-card button">
+        <Close />
+      </button>
     </div>
   );
 };
