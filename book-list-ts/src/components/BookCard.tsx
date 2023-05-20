80df2f662ad225b0433a9e2d29da1f4af13e9db4
Henrique <henriquennoliveira@gmail.com>
2023-05-19 21:52:17 -0300

    feat: implement edit event on book edit book-list-ts

--- book-list-ts/src/components/BookCard.tsx
+++ book-list-ts/src/components/BookCard.tsx
@@ -1,19 +1,42 @@
-import { FC } from "react";
+import { FC, MouseEvent, useState } from "react";
 import "../App.css";
 import { Book } from "../types";
+import BookEdit from "./BookEdit";
 
 export interface IBookCard {
   book: Book;
 }
 
 const BookCard: FC<IBookCard> = ({ book }) => {
+  const [enableEditMode, setEnableEditMode] = useState<boolean>(false);
+
+  const handleEditClick = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    setEnableEditMode(true);
+  };
+
   return (
     <div className="book-card card">
       <div className="book-card button-row">
-        <button className="book-card button">Edit</button>
+        <button className="book-card button" onClick={handleEditClick}>
+          Edit
+        </button>
         <button className="book-card button">Close</button>
       </div>
-      <div className="book-card content">{book.title}</div>
+      <div className="book-card content">
+        {enableEditMode ? (
+          <BookEdit
+            currentTitle={book.title}
+            onSave={() => {
+              setEnableEditMode(false);
+            }}
+          />
+        ) : (
+          book.title
+        )}
+      </div>
     </div>
   );
 };
--- book-list-ts/src/components/BookEdit.tsx
+++ book-list-ts/src/components/BookEdit.tsx
@@ -1,8 +1,36 @@
-import { FC } from "react";
+import { ChangeEvent, FC, FormEvent, useState, MouseEvent } from "react";
 import "../App.css";
 
-const BookEdit: FC = () => {
-  return <div>BookEdit</div>;
+interface IBookEdit {
+  onSave: (title: string) => void;
+  currentTitle: string;
+}
+
+const BookEdit: FC<IBookEdit> = ({ onSave, currentTitle }) => {
+  const [newTitle, setNewTitle] = useState(currentTitle);
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    setNewTitle(event.target.value);
+  };
+
+  const handleSubmit = (
+    event: FormEvent<HTMLFormElement> | MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    onSave(newTitle);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={onChange} value={newTitle} />
+      <button onClick={handleSubmit}>Save</button>
+    </form>
+  );
 };
 
 export default BookEdit;
