1e0c6422653db191b3f5135c68697508e8b15bd8
Henrique <henriquennoliveira@gmail.com>
2023-05-27 22:17:51 -0300

    feat: refact to use json server book-list-ts

--- book-list-ts/src/App.tsx
+++ book-list-ts/src/App.tsx
@@ -1,40 +1,70 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import "./App.css";
 import CreateBook from "./components/CreateBook";
 import { Book } from "./types";
 import BookCard from "./components/BookCard";
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3001/books/";
 
 const App: FC = () => {
   const [books, setBooks] = useState<Array<Book>>([]);
+  const [update, setUpdate] = useState<Boolean>(false);
 
-  const insertBook = (book: Book): void => {
-    const updatedBooks = [...books, book];
+  const getBooks = async (): Promise<void> => {
+    const response = await axios.get(BASE_URL);
 
-    setBooks(updatedBooks);
+    setBooks(response.data);
   };
 
-  const deleteBook = (id: string): void => {
-    let newBooks = [];
+  useEffect(() => {
+    getBooks();
+  }, [update]);
 
-    for (const book of books) {
-      if (book.id !== id) {
-        newBooks.push(book);
-      }
-    }
-
-    setBooks(newBooks);
-  };
-
-  const editBook = (id: string, newTitle: string): void => {
-    const editedBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, title: newTitle };
-      }
-
-      return book;
+  const insertBook = async (book: Book): Promise<void> => {
+    await axios.post(BASE_URL, {
+      title: book.title,
     });
 
-    setBooks(editedBooks);
+    // const updatedBooks = [...books, book];
+
+    // setBooks(updatedBooks);
+
+    setUpdate(!update);
+  };
+
+  const deleteBook = async (id: string): Promise<void> => {
+    await axios.delete(BASE_URL + id);
+
+    // let newBooks = [];
+
+    // for (const book of books) {
+    //   if (book.id !== id) {
+    //     newBooks.push(book);
+    //   }
+    // }
+
+    // setBooks(newBooks);
+
+    setUpdate(!update);
+  };
+
+  const editBook = async (id: string, newTitle: string): Promise<void> => {
+    await axios.put(BASE_URL + id, {
+      title: newTitle,
+    });
+
+    // const editedBooks = books.map((book) => {
+    //   if (book.id === id) {
+    //     return { ...book, title: newTitle };
+    //   }
+
+    //   return book;
+    // });
+
+    // setBooks(editedBooks);
+
+    setUpdate(!update);
   };
 
   return (
