8eced0f1f93e5a8667d7ecd5718cdaebbf6ea94c
Henrique <henriquennoliveira@gmail.com>
2023-05-31 21:20:49 -0300

    feat: refactor book-list-ts to center logic in context api

--- book-list-ts/src/App.tsx
+++ book-list-ts/src/App.tsx
@@ -6,69 +6,8 @@ import BookCard from "./components/BookCard";
 import axios from "axios";
 import BookContext from "./contexts/BooksContext";
 
-const BASE_URL = "http://localhost:3001/books/";
-
 const App: FC = () => {
   const context = useContext(BookContext);
-
-  const [books, setBooks] = useState<Array<Book>>([]);
-  const [update, setUpdate] = useState<Boolean>(false);
-
-  const getBooks = async (): Promise<void> => {
-    const response = await axios.get(BASE_URL);
-
-    setBooks(response.data);
-  };
-
-  useEffect(() => {
-    getBooks();
-  }, [update]);
-
-  const insertBook = async (book: Book): Promise<void> => {
-    await axios.post(BASE_URL, {
-      title: book.title,
-    });
-
-    // const updatedBooks = [...books, book];
-
-    // setBooks(updatedBooks);
-
-    setUpdate(!update);
-  };
-
-  const deleteBook = async (id: string): Promise<void> => {
-    await axios.delete(BASE_URL + id);
-
-    // let newBooks = [];
-
-    // for (const book of books) {
-    //   if (book.id !== id) {
-    //     newBooks.push(book);
-    //   }
-    // }
-
-    // setBooks(newBooks);
-
-    setUpdate(!update);
-  };
-
-  const editBook = async (id: string, newTitle: string): Promise<void> => {
-    await axios.put(BASE_URL + id, {
-      title: newTitle,
-    });
-
-    // const editedBooks = books.map((book) => {
-    //   if (book.id === id) {
-    //     return { ...book, title: newTitle };
-    //   }
-
-    //   return book;
-    // });
-
-    // setBooks(editedBooks);
-
-    setUpdate(!update);
-  };
 
   return (
     <div>
--- book-list-ts/src/contexts/BooksContext.tsx
+++ book-list-ts/src/contexts/BooksContext.tsx
@@ -1,9 +1,86 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { Book } from "../types";
+import axios from "axios";
 
 interface IBookContext {
-  value: number;
+  books: Book[];
+  setBooks: React.Dispatch<React.SetStateAction<Book[]>>;
+  update: boolean;
+  setUpdate: React.Dispatch<React.SetStateAction<Boolean>>;
+  insertBook: (book: Book) => Promise<void>;
+  deleteBook: (id: string) => Promise<void>;
+  editBook: (id: string, newTitle: string) => Promise<void>;
 }
 
+const BASE_URL = "http://localhost:3001/books/";
+
 const BookContext = createContext<IBookContext>({ value: 0 });
+
+interface IBookProvider {
+  children: JSX.Element;
+}
+
+const BookProvider: FC<IBookProvider> = ({ children }) => {
+  const [books, setBooks] = useState<Array<Book>>([]);
+  const [update, setUpdate] = useState<Boolean>(false);
+
+  const getBooks = async (): Promise<void> => {
+    const response = await axios.get(BASE_URL);
+
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    getBooks();
+  }, [update]);
+
+  const insertBook = async (book: Book): Promise<void> => {
+    await axios.post(BASE_URL, {
+      title: book.title,
+    });
+
+    // const updatedBooks = [...books, book];
+
+    // setBooks(updatedBooks);
+
+    setUpdate(!update);
+  };
+
+  const deleteBook = async (id: string): Promise<void> => {
+    await axios.delete(BASE_URL + id);
+
+    // let newBooks = [];
+
+    // for (const book of books) {
+    //   if (book.id !== id) {
+    //     newBooks.push(book);
+    //   }
+    // }
+
+    // setBooks(newBooks);
+
+    setUpdate(!update);
+  };
+
+  const editBook = async (id: string, newTitle: string): Promise<void> => {
+    await axios.put(BASE_URL + id, {
+      title: newTitle,
+    });
+
+    // const editedBooks = books.map((book) => {
+    //   if (book.id === id) {
+    //     return { ...book, title: newTitle };
+    //   }
+
+    //   return book;
+    // });
+
+    // setBooks(editedBooks);
+
+    setUpdate(!update);
+  };
+
+  return <BookContext.Provider value>{children}</BookContext.Provider>;
+};
 
 export default BookContext;
