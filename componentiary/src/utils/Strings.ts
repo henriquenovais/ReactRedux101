8523e5121e145f4ae933ec62bc45cb47e12876b1
Henrique <henriquennoliveira@gmail.com>
2023-06-09 22:14:10 -0300

    feat: create classname function

--- componentiary/src/utils/Strings.ts
+++ componentiary/src/utils/Strings.ts
@@ -1,3 +1,18 @@
-export const joinSpaced = (...input: string[]): string => {
-  return input.join(" ");
+export const className = (
+  input: string,
+  conditions: Record<string, Boolean>
+): string => {
+  const properties: string[] = Object.keys(conditions).map(
+    (propertie, index) => {
+      if (conditions[propertie]) {
+        return propertie;
+      }
+
+      return "";
+    }
+  );
+
+  properties.unshift(input);
+
+  return properties.join(" ");
 };
