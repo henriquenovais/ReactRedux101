4ad7fd56c74176eff3c9e1a1f8238f493e005103
Henrique <henriquennoliveira@gmail.com>
2023-06-10 11:34:56 -0300

    feat: creating super generic button page componentiary

--- componentiary/src/App.tsx
+++ componentiary/src/App.tsx
@@ -1,7 +1,20 @@
 import { FC } from "react";
+import GenericButton from "./components/GenericButton";
+import { ButtonColoring } from "./constants/enums/button";
 
 const App: FC = () => {
-  return <h1 className="text-3xl font-bold underline">App</h1>;
+  return (
+    <div>
+      <h3 className="text-3xl font-bold">Buttons</h3>
+      <div className="flex flex-col gap-3">
+        <GenericButton text="Click me!" coloring={ButtonColoring.PRIMARY} />
+        <GenericButton text="Buy now!" coloring={ButtonColoring.SECONDARY} />
+        <GenericButton text="See deal!" coloring={ButtonColoring.SUCCESS} />
+        <GenericButton text="Hide ads!" coloring={ButtonColoring.WARNING} />
+        <GenericButton text="Fix this!" coloring={ButtonColoring.DANGER} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- componentiary/src/components/GenericButton.tsx
+++ componentiary/src/components/GenericButton.tsx
@@ -3,6 +3,7 @@ import { ButtonColoring, ButtonShape } from "../constants/enums/button";
 import { className } from "../utils/Strings";
 
 interface IGenericButton {
+  text: string;
   shape?: ButtonShape;
   coloring?: ButtonColoring;
   isOutlined?: Boolean;
@@ -10,14 +11,27 @@ interface IGenericButton {
 }
 
 const GenericButton: FC<IGenericButton> = ({
-  shape = ButtonShape.ROUNDED_CORNERS,
+  text,
+  shape = ButtonShape.SQUARE,
   coloring = ButtonColoring.PRIMARY,
   isOutlined = true,
   isFilled = true,
 }) => {
-  const classname = className("py-1.5 px-1.5", {});
+  const classname = className("py-1.5 px-1.5 border w-32 font-bold", {
+    "border-blue-500 bg-blue-500 text-white":
+      coloring === ButtonColoring.PRIMARY,
+    "border-gray-900 bg-gray-900 text-white":
+      coloring === ButtonColoring.SECONDARY,
+    "border-green-500 bg-green-500 text-white":
+      coloring === ButtonColoring.SUCCESS,
+    "border-yellow-400 bg-yellow-400 text-white":
+      coloring === ButtonColoring.WARNING,
+    "border-red-500 bg-red-500 text-white": coloring === ButtonColoring.DANGER,
+    "rounded-full": shape === ButtonShape.PILL,
+    "rounded-lg": shape === ButtonShape.ROUNDED_CORNERS,
+  });
 
-  return <button></button>;
+  return <button className={classname}>{text}</button>;
 };
 
 export default GenericButton;
