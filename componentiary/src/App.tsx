152a9873e1d2bc263612aa3bb24f94f887aeb7b3
Henrique <henriquennoliveira@gmail.com>
2023-06-13 20:30:43 -0300

    feat: add buttons page componentiary

--- componentiary/src/App.tsx
+++ componentiary/src/App.tsx
@@ -1,23 +1,11 @@
 import { FC } from "react";
-import GenericButton from "./components/GenericButton";
-import { ButtonColoring } from "./constants/enums/button";
-import { GoBell } from "react-icons/go";
+import ButtonsPage from "./pages/Buttons";
 
 const App: FC = () => {
   return (
     <div>
-      <h3 className="text-3xl font-bold">Buttons</h3>
-      <div className="flex flex-col gap-3">
-        <GenericButton
-          text="Click me!"
-          icon={<GoBell />}
-          coloring={ButtonColoring.PRIMARY}
-        />
-        <GenericButton text="Buy now!" coloring={ButtonColoring.SECONDARY} />
-        <GenericButton text="See deal!" coloring={ButtonColoring.SUCCESS} />
-        <GenericButton text="Hide ads!" coloring={ButtonColoring.WARNING} />
-        <GenericButton text="Fix this!" coloring={ButtonColoring.DANGER} />
-      </div>
+      <h1>Componentiary</h1>
+      <ButtonsPage />
     </div>
   );
 };
---
+++ componentiary/src/pages/Buttons.tsx
@@ -0,0 +1,25 @@
+import { GoBell } from "react-icons/go";
+import GenericButton from "../components/GenericButton";
+import { ButtonColoring } from "../constants/enums/button";
+import { FC } from "react";
+
+const ButtonsPage: FC = () => {
+  return (
+    <div className="w-fit p-2 border-2 border-solid border-gray-950">
+      <h3 className="text-3xl font-bold">Buttons</h3>
+      <div className="flex flex-col gap-3">
+        <GenericButton
+          text="Click me!"
+          icon={<GoBell />}
+          coloring={ButtonColoring.PRIMARY}
+        />
+        <GenericButton text="Buy now!" coloring={ButtonColoring.SECONDARY} />
+        <GenericButton text="See deal!" coloring={ButtonColoring.SUCCESS} />
+        <GenericButton text="Hide ads!" coloring={ButtonColoring.WARNING} />
+        <GenericButton text="Fix this!" coloring={ButtonColoring.DANGER} />
+      </div>
+    </div>
+  );
+};
+
+export default ButtonsPage;
