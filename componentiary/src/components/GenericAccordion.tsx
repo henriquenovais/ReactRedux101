86de515d4d0e3e30207a6e188cbd050b3db204ba
Henrique <henriquennoliveira@gmail.com>
2023-06-14 20:03:16 -0300

    feat: add styling to accordion componentiary

--- componentiary/src/components/GenericAccordion.tsx
+++ componentiary/src/components/GenericAccordion.tsx
@@ -1,5 +1,6 @@
 import { FC, useState } from "react";
 import { AccordionItem } from "../types/genericComponents";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 interface IGenericAccordion {
   items: AccordionItem[];
@@ -16,17 +17,31 @@ const GenericAccordion: FC<IGenericAccordion> = ({ items }) => {
   };
 
   return (
-    <span className="flex flex-col gap-1.5">
+    <div className="w-full flex flex-col gap-0.5">
       {items.map((item, index) => {
         const isExpanded = index === expanded;
         return (
-          <div key={item.id}>
-            <div onClick={(event) => onClick(event, index)}>{item.header}</div>
-            {isExpanded && <div>{item.description}</div>}
+          <div
+            key={item.id}
+            className="w-80 border-2 border-solid border-gray-950"
+          >
+            <div
+              key={item.id}
+              className="flex flex-row items-center justify-between cursor-pointer bg-green-500 p-1"
+              onClick={(event) => onClick(event, index)}
+            >
+              {item.header}
+              {isExpanded ? <GoChevronLeft /> : <GoChevronDown />}
+            </div>
+            {isExpanded && (
+              <div className="cursor-default p-1 text-left border-t-2 border-gray-950">
+                {item.description}
+              </div>
+            )}
           </div>
         );
       })}
-    </span>
+    </div>
   );
 };
 
