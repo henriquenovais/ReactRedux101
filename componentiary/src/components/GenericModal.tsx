2d139920357eb99c76eb198ef622cea3fb910de1
Henrique <henriquennoliveira@gmail.com>
2023-06-26 22:59:09 -0300

    feat: implementing modal component componentiary

--- componentiary/src/components/GenericModal.tsx
+++ componentiary/src/components/GenericModal.tsx
@@ -1,6 +1,7 @@
-import { FC } from "react";
+import { FC, MouseEvent } from "react";
 import ReactDOM from "react-dom";
 import { ModalType } from "../constants/enums/modal";
+import GenericButton from "./GenericButton";
 
 interface IGenericModal {
   type: ModalType;
@@ -8,13 +9,32 @@ interface IGenericModal {
   onConfirm: () => void;
 }
 
-const GenericModal: FC = () => {
+const GenericModal: FC<IGenericModal> = ({ type, onClose, onConfirm }) => {
   const modal = document.querySelector("#modal");
   if (!modal) return <></>;
 
+  const handleClose = (
+    event: MouseEvent<HTMLDivElement> | MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    onClose();
+  };
+
   return ReactDOM.createPortal(
-    <div className="relative bg-grey-500 opacity-80">
-      <div className="absolute w-160 inset-40 bg-white">modal!!</div>
+    <div>
+      <div
+        className="absolute inset-0 bg-gray-300 opacity-80"
+        onClick={handleClose}
+      ></div>
+      <div className="absolute inset-40 flex flex-col items-center justify-center p-10  bg-white">
+        <h1>Modal!!</h1>
+        <div className="flex flex-inline gap-2">
+          {type === ModalType.CHOICE && <GenericButton text="Confirm" />}
+          <GenericButton text="Dismiss" onClick={onClose} />
+        </div>
+      </div>
     </div>,
     modal
   );
--- componentiary/src/pages/ModalPage.tsx
+++ componentiary/src/pages/ModalPage.tsx
@@ -1,14 +1,24 @@
 import { FC, useState } from "react";
 import GenericButton from "../components/GenericButton";
 import GenericModal from "../components/GenericModal";
+import { ModalType } from "../constants/enums/modal";
 
 const ModalPage: FC = () => {
   const [toggleModal, setToggleModal] = useState<boolean>(false);
 
   return (
     <div>
-      <GenericButton text="Open Modal" onClick={() => setToggleModal(true)} />
-      {toggleModal && <GenericModal />}
+      <GenericButton
+        text="Open Modal"
+        onClick={() => setToggleModal((current) => !current)}
+      />
+      {toggleModal && (
+        <GenericModal
+          type={ModalType.CHOICE}
+          onClose={() => setToggleModal(false)}
+          onConfirm={() => {}}
+        />
+      )}
     </div>
   );
 };
