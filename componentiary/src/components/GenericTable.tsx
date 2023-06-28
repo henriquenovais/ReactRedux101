c681dbcbd1bd2cf586a1e3670b6d2235c8f3e623
Henrique <henriquennoliveira@gmail.com>
2023-06-27 22:15:46 -0300

    feat: create table component and page componentiary

---
+++ componentiary/src/components/GenericTable.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const GenericTable: FC = () => {
+  return <></>;
+};
+
+export default GenericTable;
---
+++ componentiary/src/pages/TablePage.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+const TablePage: FC = () => {
+  return <></>;
+};
