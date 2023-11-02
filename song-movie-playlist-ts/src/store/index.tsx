44defa54a4e32fe83561a3a02aac96a1be7278fb
Henrique Oliveira <henriquennoliveira@gmail.com>
2023-11-02 17:33:41 -0300

    feat: add further information on action functions

--- song-movie-playlist-ts/src/store/index.tsx
+++ song-movie-playlist-ts/src/store/index.tsx
@@ -24,6 +24,7 @@ const store = configureStore({
  * How to debug Redux store
  * ===================================
  */
+
 // //getting initial state
 // const startingState = store.getState();
 
@@ -34,6 +35,34 @@ const store = configureStore({
 //   type: "song/addSong",
 //   payload: "New Song!!!",
 // });
+
+// //getting final state
+// const finalState = store.getState();
+
+// console.log("Final state >>>>>>>>", finalState);
+
+/**
+ * ============================================
+ * How to access Redux action creator functions
+ * ============================================
+ */
+
+// console.log("Action creator functions >>>>>>", songsSlice.actions);
+
+/**
+ * ============================================================
+ * Using an action creator function to change state directly
+ * ============================================================
+ */
+
+// //getting initial state
+// const startingState = store.getState();
+
+// console.log("Starting state >>>>>>>>", startingState);
+
+// //changing state through action creator function --- not recommended
+// //this is just for debugging / knowledge gathering
+// store.dispatch(songsSlice.actions.addSong("New Song!!!" as never));
 
 // //getting final state
 // const finalState = store.getState();
