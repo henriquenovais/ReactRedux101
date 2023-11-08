9c6fc175b73751c3302e592e380d424faaead47a
Henrique Oliveira <henriquennoliveira@gmail.com>
2023-11-08 01:21:59 -0300

    feat: add styling and functionality to song movie playlist

--- song-movie-playlist-ts/src/App.tsx
+++ song-movie-playlist-ts/src/App.tsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { store } from "./store";
+import SongPlaylist from "./components/SongPlaylist";
+import MoviePlaylist from "./components/MoviePlaylist";
 
 function App() {
+  const [songs, setSongs] = useState<Array<string>>([]);
+  const [movies, setMovies] = useState<Array<string>>([]);
+
+  const handleResetClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    setSongs([]);
+    setMovies([]);
+  };
+
   return (
-    <div className="App">
-      <div>REALLY COOL GUYS</div>
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col items-center justify-center w-full h-16">
+        <button
+          className="w-48 h-8 bg-orange-700 rounded-lg text-center text-xl text-white font-bold"
+          onClick={(e) => handleResetClick(e)}
+        >
+          Reset playlists
+        </button>
+      </div>
+
+      <div className="h-0.5 w-full bg-blue-900" />
+      <SongPlaylist
+        songs={songs}
+        addSong={(value) => setSongs((current) => current.concat([value]))}
+      />
+      <MoviePlaylist
+        movies={movies}
+        addMovie={(value) => setMovies((current) => current.concat([value]))}
+      />
     </div>
   );
 }
---
+++ song-movie-playlist-ts/src/components/SongPlaylist.tsx
@@ -0,0 +1,84 @@
+import { FC, useState } from "react";
+
+interface ISongPlaylist {
+  addSong: (input: string) => void;
+  songs: string[];
+}
+
+const SongPlaylist: FC<ISongPlaylist> = ({ addSong, songs }) => {
+  const [isAddSong, setIsAddSong] = useState<boolean>(false);
+  const [songName, setSongName] = useState<string>("");
+
+  const handleAddSongClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    setIsAddSong(true);
+  };
+
+  const handleNewSongChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    setSongName(event.target.value);
+  };
+
+  const handleSubmit = (
+    event:
+      | React.MouseEvent<HTMLButtonElement, MouseEvent>
+      | React.FormEvent<HTMLFormElement>
+  ): void => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    addSong(songName);
+    setSongName("");
+    setIsAddSong(false);
+  };
+
+  return (
+    <div className="w-fill flex flex-col items-center gap-3">
+      <div className="w-fill flex flex-row items-center justify-center gap-3">
+        <h1 className="text-4xl">Songs Playlist</h1>
+        {!isAddSong && (
+          <button
+            className="w-24 bg-orange-700 rounded-lg text-center text-white font-bold"
+            onClick={(e) => handleAddSongClick(e)}
+          >
+            Add song
+          </button>
+        )}
+      </div>
+      {isAddSong && (
+        <div className="w-48 flex flex-row content-center gap-3">
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <label className="font-bold">New song name:</label>
+            <input
+              className="w-48 border-2 rounded-lg border-black"
+              type="text"
+              onChange={(e) => handleNewSongChange(e)}
+              value={songName}
+            />
+            <button
+              className="w-48 h-8 bg-orange-700 rounded-lg text-center text-white font-bold"
+              onClick={(e) => handleSubmit(e)}
+            >
+              Add new song
+            </button>
+          </form>
+        </div>
+      )}
+      <div className="flex flex-col items-center">
+        {songs.map((current) => (
+          <span>{current}</span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SongPlaylist;
