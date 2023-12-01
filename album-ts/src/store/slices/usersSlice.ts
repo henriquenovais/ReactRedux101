ed594108bc644a2f24096a4bc51f3255784d4631
Henrique Oliveira <henriquennoliveira@gmail.com>
2023-11-30 21:32:13 -0300

    feat: implement react redux store with thunks with typescript album-ts

--- album-ts/src/store/slices/usersSlice.ts
+++ album-ts/src/store/slices/usersSlice.ts
@@ -1,9 +1,45 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunks/fetchUsers";
+import { User } from "../../types";
+import { UnknownAsyncThunkRejectedAction } from "@reduxjs/toolkit/dist/matchers";
+
+export interface UsersState {
+  data: Array<User>;
+  isLoading: boolean;
+  errors: Array<string>;
+}
+
+const initialState: UsersState = { data: [], isLoading: false, errors: [] };
 
 const usersSlice = createSlice({
   name: "users",
-  initialState: { data: [] },
+  initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(
+        fetchUsers.pending,
+        (state: UsersState, action: PayloadAction) => {
+          state.isLoading = true;
+        }
+      )
+      .addCase(
+        fetchUsers.fulfilled,
+        (state: UsersState, action: PayloadAction<User[]>) => {
+          state.isLoading = false;
+          state.data = action.payload;
+        }
+      )
+      .addCase(
+        fetchUsers.rejected,
+        (state: UsersState, action: UnknownAsyncThunkRejectedAction) => {
+          state.isLoading = false;
+          state.errors.push(
+            action.error.message ?? "No error message provided"
+          );
+        }
+      );
+  },
 });
 
 export const usersReducer = usersSlice.reducer;
---
+++ album-ts/src/store/thunks/fetchUsers.ts
@@ -0,0 +1,14 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { User } from "../../types";
+
+const fetchUsers = createAsyncThunk(
+  "users/fetch",
+  async (): Promise<User[]> => {
+    const response = await axios.get("http://localhost:3000/users");
+
+    return response.data;
+  }
+);
+
+export { fetchUsers };
