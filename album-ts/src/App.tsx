d0f9b7dfe403ae7b185e2ce32b28db8fc70e2bc2
Henrique Oliveira <henriquennoliveira@gmail.com>
2023-11-30 21:59:10 -0300

    feat: type gymnastics for store dispatch using async thunks album-ts

--- album-ts/src/App.tsx
+++ album-ts/src/App.tsx
@@ -1,5 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, useAppDispatch } from "./store";
+import { useEffect } from "react";
+import { fetchUsers } from "./store/thunks/fetchUsers";
+import { AsyncThunkAction, ThunkAction, ThunkDispatch } from "@reduxjs/toolkit";
+
 function App() {
-  return <h1>App</h1>;
+  const dispatch = useAppDispatch();
+  const users = useSelector((state: RootState) => {
+    return state.users;
+  });
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  console.log("users >>>>>>>>>>>>>>>>>>>>", users);
+
+  return !users.isLoading ? <h1>App</h1> : <h1>Loading...</h1>;
 }
 
 export default App;
--- album-ts/src/store/index.ts
+++ album-ts/src/store/index.ts
@@ -1,12 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {
+  AnyAction,
+  Store,
+  ThunkDispatch,
+  configureStore,
+} from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
+import thunkMiddleware from "redux-thunk";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 const store = configureStore({
   reducer: {
     users: usersReducer,
   },
+  middleware: [thunkMiddleware],
 });
 
+// 1. Get the root state's type from reducers
 export type RootState = ReturnType<typeof store.getState>;
+
+// 2. Create a type for thunk dispatch
+export type AppThunkDispatch = ThunkDispatch<RootState, any, AnyAction>;
+
+// 3. Create a type for store using RootState and Thunk enabled dispatch
+export type AppStore = Omit<Store<RootState, AnyAction>, "dispatch"> & {
+  dispatch: AppThunkDispatch;
+};
+
+// you can also create some redux hooks using the above explicit types
+export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export { store };
